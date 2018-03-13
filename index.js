a062f2d98e1a3299b9267eb7790a215ec87b856f
modu-john <john@moduparking.com>
2018-03-13 13:54:05 +0900

    add aws/s3

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const monad = require('./dist/monad');
 const wrapper = require('./dist/wrapper');
 const m2r = require('./dist/monad2response');
 const ddb = require('./dist/aws/ddb');
+const s3 = require('./dist/aws/s3');
 
 module.exports.monad = monad;
 module.exports.m2r = m2r;
 module.exports.axios = wrapper.axios;
 module.exports.aws = {
-    ddb
+    ddb,
+    s3
 };
