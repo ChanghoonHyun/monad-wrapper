c55adedf23bc5a91b0c9c298b8424dfd72dc9547
modu-john <john@moduparking.com>
2018-04-13 10:29:08 +0900

    add aws sqs, sns

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const wrapper = require('./dist/wrapper');
 const m2r = require('./dist/monad2response');
 const ddb = require('./dist/aws/ddb');
 const s3 = require('./dist/aws/s3');
+const sns = require('./dist/aws/sns');
+const sqs = require('./dist/aws/sqs');
 
 module.exports.monad = monad;
 module.exports.m2r = m2r;
 module.exports.axios = wrapper.axios;
 module.exports.aws = {
     ddb,
-    s3
+    s3,
+    sns,
+    sqs
 };
--- test/moand.js
+++ test/moand.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const R = require('ramda');
+const F = require('ramda-fantasy')
 const monad = require('../index').monad;
 
 describe('lib/monad', () => {
@@ -9,7 +10,7 @@ describe('lib/monad', () => {
         it('wrap', async () => {
             const log = v => should.exist(v);
             const M = monad(null, log);
-            
+
             const result = M.eitherR({ test: true });
             should.exist(result);
             should.equal(result.isRight, true);
@@ -19,7 +20,7 @@ describe('lib/monad', () => {
         it('wrap string', async () => {
             const log = v => should.exist(v);
             const M = monad(null, log);
-            
+
             const result = M.eitherR('test');
             should.exist(result);
             should.equal(result.isRight, true);
@@ -29,33 +30,33 @@ describe('lib/monad', () => {
     })
 
     describe('eitherL', () => {
-        
+
         it('wrap object', async () => {
             const M = monad();
-            
+
             const result = M.eitherL({ test: true });
             should.exist(result);
             should.equal(result.isRight, false);
             should.equal(result.value.test, true);
-        
+
         });
 
         it('wrap string', async () => {
             const M = monad();
-            
+
             const result = M.eitherL('test');
             should.exist(result);
             should.equal(result.isRight, false);
-            should.equal(result.value, 'test');        
+            should.equal(result.value, 'test');
         });
 
         it('wrap Error', async () => {
             const M = monad();
-            
+
             const result = M.eitherL(new Error('test'));
             should.exist(result);
             should.equal(result.isRight, false);
-            should.equal(result.value, 'test');    
+            should.equal(result.value, 'test');
         });
 
     })
@@ -66,16 +67,16 @@ describe('lib/monad', () => {
             const M = monad();
             const r = M.eitherR({ test: true });
             const result = M.eitherMap(r);
-            
+
             should.exist(result);
             should.equal(result.test, true);
         })
-        
+
         it('map Left', async () => {
             const M = monad();
             const r = M.eitherL(new Error('test'));
             const result = M.eitherMap(r);
-            
+
             should.exist(result);
             should.equal(result, 'test');
         })
@@ -88,20 +89,51 @@ describe('lib/monad', () => {
             const M = monad();
             const r = M.eitherR({ test: true });
             const result = M.ifRight(R.identity, r);
-            
+
             should.exist(result);
             should.equal(result.test, true);
         })
-        
+
         it('Left', async () => {
             const M = monad();
             const r = M.eitherL(new Error('test'));
             const result = M.ifLeft(R.identity, r);
-            
+
             should.exist(result);
             should.equal(result, 'test');
         })
 
     })
+
+    const val1 = {
+        name: 'hyun'
+    };
+    // const maybeChain = func => R.pipe(func, F.Maybe);
+    // const Maybe = F.Maybe;
+    // const maybePipe = (args) => R.pipeK(Maybe, ...R.map(maybeChain, args));
+    console.log(maybePipe([
+        R.tap(console.log),
+        R.prop('name'),
+        R.tap(console.log),
+        R.concat('aa'),
+        R.tap(console.log)
+    ])(val1).isJust);
+    // console.log(pipeK([
+    //     R.tap(console.log),
+    //     R.prop('name'),
+    //     R.tap(console.log),
+    //     R.concat('aa'),
+    //     R.tap(console.log)
+    // ])(val1).isJust);
+    // console.log(pipeK([
+    //     R.tap(console.log),
+    //     R.prop('name'),
+    //     R.tap(console.log),
+    //     R.concat('aa'),
+    //     R.tap(console.log),
+    //     R.prop('test'),
+    //     R.prop('test'),
+    //     R.tap(console.log)
+    // ])(null).isJust);
 
 });
